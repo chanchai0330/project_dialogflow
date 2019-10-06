f5024ee2e11ebb51390d00126c7a172ed0ef4a08
chanchai0330 <54763811+chanchai0330@users.noreply.github.com>
2019-10-06 19:54:55 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+function  getSpreadsheetData(){
+     return axios.get('https://sheetdb.io/api/v1/eodqzgdxnx66z');
+   }
+  
+
+function QuestionHandler(agent){
+    const text = agent.parameters.text;
+    return getSpreadsheetData().then(res => {
+      	res.data.map(person => {
+          if(person.Question === text )
+           agent.add(` ${person.Answer}`);
+          	
+        });
+    });
